0af77101311fb6c2b7250a989b155736ad6670b8
sweet_winter <twins1040@gmail.com>
2018-12-02 23:18:30 +0900

    Clean features.js

--- static/bypass/features.js
+++ static/bypass/features.js
@@ -1,7 +1,120 @@
-var login_url = '/login/google-oauth2/';
-var logout_url = '/logout/';
-var sample_background_url = "static/img/blue_furniture_resize.jpg";
+//
+// GLOBAL VARIABLES
+//
+var LOGIN_URL = '/login/google-oauth2/';
+var LOGOUT_URL = '/logout/';
+var SAMPLE_BACKGROUND_URL = "static/img/blue_furniture_resize.jpg";
+var HISTORY_MAX_LEN = 50;
+var FONTS = ["Noto Sans KR", "Nanum Gothic", "Nanum Myeongjo", "Hanna", "Poor Story"];
+var GRADIENTS = [{name: "red orange", h:0, v:1, stops:{0:"red", 1:"orange"}},
+				 {name: "blue darkblue", h:0, v:1, stops:{0:"blue", 1:"darkblue"}},
+				 {name: "red darkred", h:0, v:1, stops:{0:"red", 1:"darkred"}}];
 
+var canvas = new fabric.Canvas('myCanvas');
+
+// Color picker
+var fillElem = $("#fill-hueb")[0];
+var strokeElem = $("#stroke-hueb")[0];
+var fillHue = new Huebee(fillElem, {setText:false});
+var strokeHue = new Huebee(strokeElem, {setText:false});
+
+// For Add text
+var sampleText = new fabric.IText("Double Click to edit!", {
+	fontFamily: 'Noto Sans KR',
+	fontSize: 50,
+	fontWeight: 900,
+	fill: 'white',
+	stroke: 'black',
+	strokeWidth:20,
+	paintFirst: 'stroke',
+	charSpacing: -100,
+	angle:  0,
+	top: canvas.height/2,
+	left: canvas.width/2,
+	originX: 'center',
+	originY: 'center',
+});
+
+var History = new function() {
+	var _work_history = [];
+	var _history_head = 0;
+
+	this.add = function() {
+		var snap = JSON.stringify(canvas);
+
+		// Make head 0
+		while (_history_head !=0) {
+			_work_history.shift();
+			_history_head -= 1;
+		}
+
+		_work_history.unshift(snap);
+
+		if (_work_history.length > HISTORY_MAX_LEN) {
+			_work_history.pop();
+		}
+
+		console.log("add history");
+	}
+
+	// Undo
+	this.undo = function() {
+		if (_history_head < _work_history.length - 1) {
+			_history_head += 1;
+		}
+
+		canvas.loadFromJSON(_work_history[_history_head]);
+
+		console.log('undo');
+	}
+
+	// Redo
+	this.redo = function() {
+		if (_history_head > 0) {
+			_history_head -= 1;
+		}
+
+		canvas.loadFromJSON(_work_history[_history_head]);
+
+		console.log('redo');
+	}
+}
+
+
+
+//
+// CANVAS SETTINGS
+//
+canvas.setDimensions({width:1280, height:720}, {backstoreOnly:true});
+canvas.selection = true;
+
+canvas.on("mouse:up", function(opt) {console.log(opt)});
+canvas.on("selection:created", setTextAttr);
+canvas.on("selection:updated", setTextAttr);
+canvas.on("object:modified", History.add);
+
+// Controller design setting
+fabric.Object.prototype.set({
+	transparentCorners: false,
+	cornerColor: 'white',
+	cornerStrokeColor: "#69F",
+	borderColor: "#69F",
+	cornerSize: 20,
+	borderScaleFactor: 4,	// controller border width
+	padding: 2,
+	cornerStyle: 'circle',
+});
+
+// Remove middle point of controller
+fabric.Object.prototype.setControlsVisibility({
+	mb: false, ml: false, mr: false, mt: false
+});
+
+
+
+//
+// FUNCTIONS
+//
 function isLogin() {
 	var b = $("#switch-user").attr("data-user");
 
@@ -52,51 +165,6 @@ function Paste() {
 	});
 }
 
-// Work history
-var add_history;
-var redo_work;
-var undo_work;
-
-(function() {
-	var work_history = [];
-	var history_max_len = 50;
-	var history_head = 0;
-
-	add_history = function() {
-		snap = JSON.stringify(canvas);
-		// Make head 0
-		while (history_head !=0) {
-			console.log("while");
-			work_history.shift();
-			history_head -= 1;
-			console.log(history_head);
-		}
-		work_history.unshift(snap);
-		if (work_history.length > history_max_len) {
-			work_history.pop();
-		}
-		console.log("modified");
-	}
-
-	// Undo
-	undo_work = function() {
-		console.log('undo');
-		if (history_head < work_history.length - 1) {
-			history_head += 1;
-		}
-		canvas.loadFromJSON(work_history[history_head]);
-	}
-
-	// Redo
-	redo_work = function() {
-		console.log('redo');
-		if (history_head > 0) {
-			history_head -= 1;
-		}
-		canvas.loadFromJSON(work_history[history_head]);
-	}
-})();
-
 function activeObjectSet(callback) {
 	var actobj = canvas.getActiveObject();
 
@@ -111,10 +179,9 @@ function activeObjectSet(callback) {
 		canvas.renderAll();
 
 		// Save work history
-		add_history();
+		History.add();
 	}
 }
-
 
 function load_template(json) {
 	var pjson = JSON.parse(json);
@@ -133,9 +200,8 @@ function load_template(json) {
 		canvas.renderAll();
 	});
 
-	add_history();
+	History.add();
 }
-
 
 function save_session(e, callback) {
 	var token = $("input[name='csrfmiddlewaretoken']").attr("value");
@@ -152,7 +218,6 @@ function save_session(e, callback) {
 	});
 }
 
-
 function restore_session(json) {
 	var pjson = JSON.parse(json);
 
@@ -168,7 +233,6 @@ function clear_session() {
 		data: {data: "", csrfmiddlewaretoken: token},
 	});
 }
-
 
 function set_background_image(src) {
 	var imgObj = new Image();
@@ -188,10 +252,9 @@ function set_background_image(src) {
 			originY: 'center'
 		});
 
-		add_history();
+		History.add();
 	}
 }
-
 
 function add_image(src) {
 	var imgObj = new Image();
@@ -200,30 +263,23 @@ function add_image(src) {
 		var image = new fabric.Image(imgObj);
 
 		canvas.add(image);
-		add_history();
+		History.add();
 	}
 }
-
 
 function group_align(direction, opt) {
 	var options = ["left", "center", "right", "top", "bottom"];
 	var directions = ["originX", "originY"];
 	var actobj, key, value, distence;
 
-	if (!options.includes(opt)) {
-		return;
-	}
-
-	if (!directions.includes(direction)) {
+	if (!options.includes(opt) ||
+		!directions.includes(direction)) {
 		return;
 	}
 
 	actobj = canvas.getActiveObject();
-	if (!actobj) {
-		return;
-	}
 
-	if (actobj.type != 'activeSelection') {
+	if (!actobj || actobj.type != 'activeSelection') {
 		return;
 	}
 
@@ -245,21 +301,10 @@ function group_align(direction, opt) {
 	}
 
 	activeObjectSet(function(obj) {
-		console.log(direction+opt+key+value);
 		obj.set(direction, opt);
 		obj.set(key, value);
 	});
 }
-
-
-
-// Canvas start!!
-var canvas = new fabric.Canvas('myCanvas');
-
-canvas.setDimensions({width:1280, height:720}, {backstoreOnly:true});
-canvas.selection = true;
-canvas.on("object:modified", add_history);
-canvas.on("mouse:up", function(opt) {console.log(opt)});
 
 // Set values on text selection
 function setTextAttr(_obj) {
@@ -277,155 +322,59 @@ function setTextAttr(_obj) {
 	$("#sliderTextStroke").attr("value", obj.strokeWidth);
 }
 
-canvas.on("selection:created", setTextAttr);
-canvas.on("selection:updated", setTextAttr);
-
-// Get value of slider and set text
-$("#sliderFontSize").on("input", function() {
-	var actobj = canvas.getActiveObject();
-	actobj.fontSize = $(this).val();
-	canvas.renderAll();
-	console.log( $(this).val());
-	add_history();
-});
-$("#sliderTextStroke").on("input", function() {
-	var actobj = canvas.getActiveObject();
-	actobj.strokeWidth = $(this).val();
-	canvas.renderAll();
-	add_history();
-});
-
-// Controller design setting
-fabric.Object.prototype.set({
-	transparentCorners: false,
-	cornerColor: 'white',
-	cornerStrokeColor: "#69F",
-	borderColor: "#69F",
-	cornerSize: 20,
-	borderScaleFactor: 4,	// controller border width
-	padding: 2,
-	cornerStyle: 'circle',
-});
-
-// Remove middle point of controller
-fabric.Object.prototype.setControlsVisibility({
-	mb: false, ml: false, mr: false, mt: false
-});
 
 
-// Example for initial canvas
-var ubuntuText = new fabric.IText("템플릿을 골라보세요!", {
-	fontFamily: 'Noto Sans KR',
-	fontSize: 100,
-	fontWeight: 900,
-	fill: '#F00',
-	stroke: 'black',
-	strokeWidth:20,
-	paintFirst: 'stroke',
-	charSpacing: -100,
-	angle:  0,
-	top: canvas.height/2,
-	left: canvas.width/2,
-	originX: 'center',
-	originY: 'center',
-});
-
-// For Add text
-var sampleText = new fabric.IText("Double Click to edit!", {
-	fontFamily: 'Noto Sans KR',
-	fontSize: 50,
-	fontWeight: 900,
-	fill: 'white',
-	stroke: 'black',
-	strokeWidth:20,
-	paintFirst: 'stroke',
-	charSpacing: -100,
-	angle:  0,
-	top: canvas.height/2,
-	left: canvas.width/2,
-	originX: 'center',
-	originY: 'center',
-});
-canvas.add(ubuntuText);
-
-// Button event except color picker
-$("#btn-undo").click(undo_work);
-$("#btn-redo").click(redo_work);
-$("#btn-copy").click(function() {Copy(); Paste()});
-$("#btn-delete").click(function(){
-	activeObjectSet(function(obj) {canvas.remove(obj)});
-});
-$("#stroke-thicker").click(function() {
-	activeObjectSet(function(obj){obj.set("strokeWidth", obj.strokeWidth+4)});
-});
-$("#stroke-thinner").click(function() {
-	activeObjectSet(function(obj){
-		step = 4;
-		if (obj.strokeWidth >= 4) {
-			obj.set("strokeWidth", obj.strokeWidth-4);
-		}
-	});
-});
-$("#stroke-delete").click(function() {
-	activeObjectSet(function(obj){obj.set("strokeWidth", 0)});
-});
-
-var gradients = [{name: "red orange", h:0, v:1, stops:{0:"red", 1:"orange"}},
-		 {name: "blue darkblue", h:0, v:1, stops:{0:"blue", 1:"darkblue"}},
-		 {name: "red darkred", h:0, v:1, stops:{0:"red", 1:"darkred"}}];
-
-
-// Color picker event
-var fillElem = $("#fill-hueb")[0];
-var strokeElem = $("#stroke-hueb")[0];
-var fillHue = new Huebee(fillElem, {setText:false});
-var strokeHue = new Huebee(strokeElem, {setText:false});
-
+//
+// EVENT HANDLERS
+//
 fillHue.on('change', function(color) {
 	if($(".huebee").length !== 0) {
 		console.log('fill '+color);
 		activeObjectSet(function(obj) {obj.set("fill", color)});
 	}
 });
-
 strokeHue.on('change', function(color) {
 	if($(".huebee").length !== 0) {
 		console.log('stroke '+color);
 		activeObjectSet(function(obj) {obj.set("stroke", color)});
 	}
 });
-
-// Font selector
-var fonts = ["Noto Sans KR", "Nanum Gothic", "Nanum Myeongjo", "Hanna", "Poor Story"];
-fonts.forEach(function(font) {
-	e = document.createElement('a');
-	$(e).addClass("dropdown-item");
-	$(e).addClass("dropdown-item-font");
-	$(e).html(font);
-	$(e).click(function() {
-		activeObjectSet(function(obj) {
-			obj.set("fontFamily", font);
-		});
-	});
-	$("#font-dropdown-menu").append(e);
+// DOM events
+$("#btn-undo").click(History.undo);
+$("#btn-redo").click(History.redo);
+$("#btn-copy").click(function() {Copy(); Paste()});
+$("#btn-delete").click(function(){
+	activeObjectSet(function(obj) {canvas.remove(obj)});
 });
-
-
-// Event for download btn
+$("#stroke-delete").click(function() {
+	activeObjectSet(function(obj){obj.set("strokeWidth", 0)});
+});
+// Get value of slider and set text
+$("#sliderFontSize").on("input", function() {
+	var actobj = canvas.getActiveObject();
+	actobj.fontSize = $(this).val();
+	canvas.renderAll();
+	console.log( $(this).val());
+	History.add();
+});
+$("#sliderTextStroke").on("input", function() {
+	var actobj = canvas.getActiveObject();
+	actobj.strokeWidth = $(this).val();
+	canvas.renderAll();
+	History.add();
+});
 $("#download-btn-a").click(function(ev) {
 	if (isLogin()) {
 		// Download Image
 		this.href = canvas.toDataURL();
 		this.download = "mypainting.png";
 	} else {
-		alert("다운로드는 로그인 후 가능합니다.");
+		alert("Please Log In");
 		save_session(null, function() {
-			location.href = login_url;
+			location.href = LOGIN_URL;
 		});
 	}
 });
-
-
 $("#add-my-template").click(function(ev) {
 	if (isLogin()) {
 		// Upload template
@@ -441,89 +390,12 @@ $("#add-my-template").click(function(ev) {
 			$('#upload-tmpl-form').submit();
 		});
 	} else {
-		alert("로그인 후 가능합니다.");
+		alert("Please Log In");
 		save_session(null, function() {
-			location.href = login_url;
+			location.href = LOGIN_URL;
 		});
 	}
 });
-
-
-// Create image loader btn
-document.getElementById('imgLoader').onchange = function(e) {
-	var reader = new FileReader();
-	reader.onload = function (event){
-		set_background_image(event.target.result);
-	}
-	reader.readAsDataURL(e.target.files[0]);
-}
-
-
-// Create clip loader btn
-document.getElementById('clipLoader').onchange = function(e) {
-	function readAndAdd(file) {
-		if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
-			var reader = new FileReader();
-
-			reader.onload = function (event) {
-				add_image(event.target.result);
-			}
-			reader.readAsDataURL(file);
-		}
-	}
-
-	if (e.target.files) {
-		[].forEach.call(e.target.files, readAndAdd);
-	}
-}
-
-// Temp save button event
-$('#temp-save').click(function() {
-	save_session();
-	alert("임시저장 되었습니다.");
-});
-
-// Scroll evented
-/*
-$(window).scroll(function(){
-   var s = $(window).scrollTop();
-   var b = $(".block-canvas");
-   if ( s > 100 ) {
-	   b.addClass("sticky-header");
-   } else {
-	   b.removeClass("sticky-header");
-   }
-});
-*/
-
-
-// Key Binding
-$(window).keydown(function(e){
-	var actobj = canvas.getActiveObject();
-
-	console.log('key: '+e.which);
-
-	// Prevent conflict with IText key shortcut
-	// If type:group, it is not being edited
-	if (actobj && actobj.type == 'i-text') {
-		if (actobj.isEditing) {
-			console.log("now editing");
-			return;
-		}
-	}
-
-	// Bind with key code
-	if(e.which === 90 && e.ctrlKey) {
-		(e.shiftKey) ? redo_work() : undo_work();
-	} else if (e.which === 46) {
-		activeObjectSet(function(obj) {canvas.remove(obj)});
-	} else if (e.which === 67 && e.ctrlKey) {
-		Copy();
-	} else if (e.which === 86 && e.ctrlKey) {
-		Paste();
-	}
-});
-
 // Thumbnail image loading and hook event
 $(".block-thumbnail").each(function(i, item) {
 	// First item is add template button
@@ -532,7 +404,6 @@ $(".block-thumbnail").each(function(i, item) {
 	}
 	// Template id
 	let id = item.id.split('-')[1];
-	console.log(id);
 
 	// Load Image
 	let img = $(item).find('img')[0];
@@ -558,57 +429,128 @@ $(".block-thumbnail").each(function(i, item) {
 		let _item = item;
 		$.get("session/", function(json) {
 			if (json === "") {
-				set_background_image(sample_background_url);
+				set_background_image(SAMPLE_BACKGROUND_URL);
 				$(_item).click();
 			} else {
-				console.log("clear, load canvas");
+				console.log("restore session");
 				restore_session(json);
 			}
 		});
 	}
 });
-
 $("#switch-user").click(function() {
 	var href, txt;
 	if (isLogin()) {
-		if(confirm("데이터가 사라집니다. 계속하시겠습니까?")) {
-			location.href = logout_url;
+		if(confirm("Data will be removed, continue?")) {
+			location.href = LOGOUT_URL;
 		}
 	} else {
 		save_session(null, function() {
-			location.href = login_url;
+			location.href = LOGIN_URL;
 		});
 	}
 });
-
-$("#align-left").click(function() {
-	group_align("originX", "left");
-});
-
-$("#align-vertical-center").click(function() {
-	group_align("originX", "center");
-});
-
-$("#align-right").click(function() {
-	group_align("originX", "right");
-});
-
-$("#align-top").click(function() {
-	group_align("originY", "top");
-});
-
-$("#align-horizontal-center").click(function() {
-	group_align("originY", "center");
-});
-
-$("#align-bottom").click(function() {
-	group_align("originY", "bottom");
-});
-
+$("#align-left").click(function() {group_align("originX", "left")});
+$("#align-vertical-center").click(function() {group_align("originX", "center")});
+$("#align-right").click(function() {group_align("originX", "right")});
+$("#align-top").click(function() {group_align("originY", "top")});
+$("#align-horizontal-center").click(function() {group_align("originY", "center")});
+$("#align-bottom").click(function() {group_align("originY", "bottom")});
 $("#addText").click(function() {
 	sampleText.clone(function(clonedObj) {
 		canvas.add(clonedObj);
 		canvas.setActiveObject(clonedObj);
-		add_history();
+		History.add();
 	});
 });
+$('#imgLoader').on('change', function(e) {
+	var reader = new FileReader();
+	reader.onload = function (event){
+		set_background_image(event.target.result);
+	}
+	reader.readAsDataURL(e.target.files[0]);
+});
+$('#clipLoader').on('change', function(e) {
+	function readAndAdd(file) {
+		if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
+			var reader = new FileReader();
+
+			reader.onload = function (event) {
+				add_image(event.target.result);
+			}
+			reader.readAsDataURL(file);
+		}
+	}
+
+	if (e.target.files) {
+		[].forEach.call(e.target.files, readAndAdd);
+	}
+});
+
+
+
+
+//
+// EDIT DOM ELEMENTS
+//
+// Font selector
+FONTS.forEach(function(font) {
+	e = document.createElement('a');
+	$(e).addClass("dropdown-item");
+	$(e).addClass("dropdown-item-font");
+	$(e).html(font);
+	$(e).click(function() {
+		activeObjectSet(function(obj) {
+			obj.set("fontFamily", font);
+		});
+	});
+	$("#font-dropdown-menu").append(e);
+});
+
+// Key Binding
+$(window).keydown(function(e){
+	var actobj = canvas.getActiveObject();
+
+	console.log('key: '+e.which);
+
+	// Prevent conflict with IText key shortcut
+	// If type:group, it is not being edited
+	if (actobj && actobj.type == 'i-text') {
+		if (actobj.isEditing) {
+			console.log("now editing");
+			return;
+		}
+	}
+
+	// Bind with key code
+	if(e.which === 90 && e.ctrlKey) {
+		(e.shiftKey) ? History.undo() : History.redo();
+	} else if (e.which === 46) {
+		activeObjectSet(function(obj) {canvas.remove(obj)});
+	} else if (e.which === 67 && e.ctrlKey) {
+		Copy();
+	} else if (e.which === 86 && e.ctrlKey) {
+		Paste();
+	}
+});
+
+// Initail text
+(function() {
+	var ubuntuText = new fabric.IText("Select Your Template!", {
+		fontFamily: 'Noto Sans KR',
+		fontSize: 100,
+		fontWeight: 900,
+		fill: '#F00',
+		stroke: 'black',
+		strokeWidth:20,
+		paintFirst: 'stroke',
+		charSpacing: -100,
+		angle:  0,
+		top: canvas.height/2,
+		left: canvas.width/2,
+		originX: 'center',
+		originY: 'center',
+	});
+
+	canvas.add(ubuntuText);
+})();
