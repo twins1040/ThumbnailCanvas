892cb4781aef576fdf0855288ad2463346bcbf89
sweet_winter <twins1040@gmail.com>
2019-02-20 18:35:15 +0900

    Add CORS allow settings

--- thumbnailCanvas/settings/development.py
+++ thumbnailCanvas/settings/development.py
@@ -10,29 +10,9 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
-"""
-ALLOWED_HOSTS = [
-    '175.113.156.193',
-    '192.168.25.50',
-    '13.209.162.217',
-    'youtube-thumbnail-maker.tk',
-    'youtube-thumbnail-maker.ga',
-    'www.thumbnail-maker.ga',
-    'thumbnail-maker.ga',
-    'www.thumbnail-maker.xyz',
-    'thumbnail-maker.xyz',
-]
-
-
-"""
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
