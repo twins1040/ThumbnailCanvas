b24414ed6b55014ab2589b64b9804a9174f40cf3
twins1040_wsl <twins1040@gmail.com>
2019-04-12 16:53:56 +0900

    add options for mobile test script

--- run_weinre.sh
+++ run_weinre.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "error: no args. ex) ./run_weinre 10.193.6.51"
-  exit
-fi
+clean=false
+weinre_host=$(ifconfig wifi0 | grep 'inet ' | sed 's/^ *//' | cut -d' ' -f 2)
 
-weinre_host=$1
-
+while getopts di: option
+do
+case "${option}"
+in
+d) clean=true;;
+i) weinre_host=${OPTARG};;
+esac
+done
 
 cat_script="cat mobile_test.patch"
 cat_r_script="cat mobile_test_reset.patch"
@@ -14,7 +18,8 @@ sed_command="sed s/Whost/$weinre_host/"
 run="patch -p0"
 dry_run="$run --dry-run"
 
-if [ "$2" == "clean" ]; then
+echo "ip adress: $weinre_host"
+if $clean; then
   if $cat_r_script | $sed_command | $dry_run; then
     $cat_r_script | $sed_command | $run
   else
